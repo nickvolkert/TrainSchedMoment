ced3fc5a15450ad05ac046a803d3fedeb617b1a1
Nicholas Volkert <nvolkert@NVs-iMac.attlocal.net>
2018-05-10 22:12:56 -0500

    adding readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Train Tracking using Moment.js
+
+### Overview
+
+This is a train tracking app that tracks train arrival in user enter increments and start time, calculated by Moment.js.
+
+### About the project
+
+This is a Javascript API assignment; part of my pursuit for the Northwestern FullStack Bootcamp Certification.
+
+The project uses the following technology:
+1. Moment.js.
+
+2. Twitter Bootstrap.
+
+3. A Bootstrap theme for styling.
+
+4. HTML, CSS and jQueryf.
+
+Like what you see? Work with me by contacting me at nick@nickvolkert.com, or view more of my work at nickvolkert.com
--- js/train.js
+++ js/train.js
@@ -18,7 +18,7 @@
 
     var trainName = $("#trainName").val().trim();
     var trainDestination = $("#destination").val().trim();
-    var trainStart = moment($("#firstTrain").val().trim(), "HH:mm").format("X");;
+    var trainStart = moment($("#firstTrain").val().trim(), "HH:mm").format("X");
     var trainFrequency = $("#frequency").val().trim();
 
     firebase.database().ref().push({
@@ -35,18 +35,18 @@
     var trainName = childSnapshot.val().name;
     var trainDestination = childSnapshot.val().destination;
     var trainStart = childSnapshot.val().start;
-    var frequency = childSnapshot.val().frequency;
+    var trainFrequency = childSnapshot.val().frequency;
 
     var difference = moment().diff(moment.unix(trainStart), "HH:mm");
-    var timeLeft = moment().diff(moment.unix(trainStart), "minutes") % frequency;
-    var mins = moment(frequency - timeLeft, "minutes").format("mm");
-    var minutesAway = moment().add(mins, "minutes").format("HH:mm a");
+    var timeLeft = moment().diff(moment.unix(trainStart), "minutes") % trainFrequency;
+    var mins = moment(trainFrequency - timeLeft, "minutes").format("mm");
+    var minutesAway = moment().add(mins, "minutes").format("HH:mm");
 
     var trainPlacement = $("#trainTable tbody");
     var trainRowCell = $("<tr>");
     var trainLineCell = $("<th class='trainLineCell'><p>" + trainName + "</p></th>");
     var trainDestCell = $("<td class='desinationCell'><p>" + trainDestination + "</p></td>");
-    var trainFreqCell = $("<td class='freqCell'><p>" + frequency + "</p></td>");
+    var trainFreqCell = $("<td class='freqCell'><p>" + trainFrequency + "</p></td>");
     var trainMinAwayCell = $("<td class='minAwayCell'><p>" + minutesAway + "</p></td>");
     var trainMinCell = $("<td class='minCell'><p>:" + mins + " mins</p></td>");
     trainRowCell.append(trainLineCell, trainDestCell, trainFreqCell, trainMinAwayCell, trainMinCell);
@@ -55,5 +55,25 @@
     $(".trainLineCell p").each(function(){
       var color = $(this).text();
       $(this).css("background-color", color);
+      // if ($(this).text() === 'yellow' || 'pink'){
+      //   $(this).css("color", "black")
+      // }
+      // if ($(".trainLineCell p").text() === 'yellow' || 'pink'){
+      //   $(this).css("color", "black", "background-color", color);
+      // } else {
+      //   $(this).css("background-color", color);
+      // }
+      // if ($(".trainLineCell p").css('background-color') === 'yellow') {
+      //   $(".trainLineCell p").css("color", "black");
+      //   console.log("background color is running");
+      // }
+      // var bg = $(".trainLineCell p").css('background-color');
+      //
+      // if (bg !== 'yellow') {
+      //     $(".trainLineCell p").css('color', 'black');
+      // }
+      // else {
+      //     $(".trainLineCell p").css('color', 'white');
+      // }
     });
   });
